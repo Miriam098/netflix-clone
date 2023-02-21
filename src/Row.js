03568605adb98a3caedbb18da2a1dd3cc136024f
Miriam Olivier <miriamolivier098@gmail.com>
2023-02-21 21:52:44 +0200

    Deployed Front-end build using Firebase

--- src/Row.js
+++ src/Row.js
@@ -24,12 +24,14 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             <h2>{title}</h2>
             <div className="row__posters">
                 {movies.map(movie => (
-                    <img
-                        className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
-                        key={movie.id}
-                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
-                            }`} alt={movie.title} />
-                ))}</div>
+                    (isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
+                        <img
+                            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                            key={movie.id}
+                            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                                }`} alt={movie.title} />
+                    )
+                )}</div>
 
         </div>
     )
